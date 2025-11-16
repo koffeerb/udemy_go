4471484f78ffb4f9a07e4213e368249981dcf65a
koffeerb <richie+gh@richardwalsh.ie>
2025-11-16 22:52:11 +0000

    Adding practice code (#1)

---
+++ practice/10/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/GoesToEleven/puppy"
+)
+
+func main() {
+	fmt.Printf(puppy.Bark() + "\n")
+	fmt.Printf(puppy.Barks() + "\n")
+	fmt.Printf(puppy.BigBark() + "\n")
+}
