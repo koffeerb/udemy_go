e8f79241f7f3732fdfeaa4d06a1571d1f47003ed
Richard <richie+gh@richardwalsh.ie>
2025-11-19 17:25:36 +0000

    Appending

--- practice/11_DataStructures/main.go
+++ practice/11_DataStructures/main.go
@@ -24,8 +24,15 @@ func main() {
 	// fmt.Println(as)
 	// fmt.Println(len(as))
 
-	food := []string{"Almond Biscotti Café", "Banana Pudding ", "Balsamic Strawberry (GF)", "Bittersweet Chocolate (GF)", "Blueberry Pancake (GF)", "Bourbon Turtle (GF)", "Browned Butter Cookie Dough", "Chocolate Covered Black Cherry (GF)", "Chocolate Fudge Brownie", "Chocolate Peanut Butter (GF)", "Coffee (GF)", "Cookies & Cream", "Fresh Basil (GF)", "Garden Mint Chip (GF)", "Lavender Lemon Honey (GF)", "Lemon Bar", "Madagascar Vanilla (GF)", "Matcha (GF)", "Midnight Chocolate Sorbet (GF, V)", "Non-Dairy Chocolate Peanut Butter (GF, V)", "Non-Dairy Coconut Matcha (GF, V)", "Non-Dairy Sunbutter Cinnamon (GF, V)", "Orange Cream (GF) ", "Peanut Butter Cookie Dough", "Raspberry Sorbet (GF, V)", "Salty Caramel (GF)", "Slate Slate Different", "Strawberry Lemonade Sorbet (GF, V)", "Vanilla Caramel Blondie", "Vietnamese Cinnamon (GF)", "Wolverine Tracks (GF)"}
+	food := []string{"Almond Biscotti Café", "Banana Pudding ", "Balsamic Strawberry (GF)"}
 	// allow to use range and ignore the key value as a return
+	for _, foodName := range food {
+		fmt.Printf("%v\n", foodName)
+	}
+	fmt.Println("There are", len(food), "flavours of ice-cream")
+	fmt.Println("----------------------------------------------")
+	food = append(food, "Chocolate", "Mint", "Rum&Raisin")
+
 	for _, foodName := range food {
 		fmt.Printf("%v\n", foodName)
 	}
