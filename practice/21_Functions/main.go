19355795984ee04ee317649d2f969935d2a9362f
Richard <richie+gh@richardwalsh.ie>
2025-11-20 11:30:01 +0000

    variadics

---
+++ practice/21_Functions/main.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+func main() {
+	foo()
+	bar("Richie")
+	s := whizz("Ali")
+	fmt.Println(s)
+	s1, n := dogYears("Ali", 38)
+	fmt.Println(s1, n)
+	x := sum(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	fmt.Println(x)
+
+}
+
+func foo() {
+	fmt.Println("From foo")
+}
+
+func bar(s string) {
+	fmt.Println("My name is", s)
+}
+
+func whizz(s string) string {
+	return fmt.Sprint("My name is ", s)
+
+}
+
+func dogYears(name string, age int) (string, int) {
+	age *= 7
+	return fmt.Sprint(name, " is this old in dog years "), age
+}
+
+func sum(ii ...int) int {
+	fmt.Println(ii)
+	fmt.Printf("%T\n", ii)
+
+	n := 0
+	for _, v := range ii {
+		n += v
+	}
+	return n
+
+}
