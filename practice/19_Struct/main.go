623f82ba2c759e5fed2e03bf6a46b78b7945b61b
Richard <richie+gh@richardwalsh.ie>
2025-11-20 11:08:46 +0000

    maps over structs

--- practice/19_Struct/main.go
+++ practice/19_Struct/main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 type person struct {
-	first string
-	last  string
-	age   int
+	first    string
+	last     string
+	age      int
+	icecream []string
 }
 
 type human struct {
@@ -16,18 +17,20 @@ type human struct {
 func main() {
 	rw := human{
 		person: person{
-			first: "Richie",
-			last:  "Walsh",
-			age:   38,
+			first:    "Richie",
+			last:     "Walsh",
+			age:      38,
+			icecream: []string{"Mint", "Pistachio"},
 		},
 		isHuman: true,
 	}
 
 	ak := human{
 		person: person{
-			first: "Ali",
-			last:  "Kavanagh",
-			age:   37,
+			first:    "Ali",
+			last:     "Kavanagh",
+			age:      37,
+			icecream: []string{"Rum&Raisin", "Lemon Sorbet"},
 		},
 		isHuman: true,
 	}
@@ -44,12 +47,26 @@ func main() {
 		species: "cat",
 	}
 
-	fmt.Println(rw)
-	fmt.Println(ak)
+	// fmt.Println(rw)
+	// fmt.Println(ak)
 	fmt.Println(tk)
 
-	fmt.Printf("%#v\n", rw)
-	fmt.Printf("%#v\n", rw.person)
-	fmt.Println(ak.isHuman)
-	fmt.Printf("%T", tk)
+	// fmt.Printf("%#v\n", rw)
+	// fmt.Printf("%#v\n", rw.person)
+	// for _, v := range ak.icecream {
+	// 	fmt.Println(ak.first, "'s favourite icecream is", v)
+	// }
+	fmt.Printf("%T\n", tk)
+
+	m := map[string]human{
+		rw.last: rw,
+		ak.last: ak,
+	}
+
+	for _, v := range m {
+		fmt.Println(v)
+		for _, v2 := range v.icecream {
+			fmt.Println(v.first, v.last, v2)
+		}
+	}
 }
